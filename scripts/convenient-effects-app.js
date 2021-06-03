85c9c812b1bcf5e011c4148612285cfb19c614e3
Derek Fredrickson <dfreds08@gmail.com>
2021-06-03 18:32:37 -0400

    Support adding favorites

--- scripts/convenient-effects-app.js
+++ scripts/convenient-effects-app.js
@@ -1,14 +1,15 @@
 import DynamicEffectsAdder from './dynamic-effects-adder.js';
+import Settings from './settings.js';
 
 export default class ConvenientEffectsApp extends Application {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       id: 'convenient-effects',
-      classes: ['fxmaster', 'sidebar-popout'],
+      classes: ['sidebar-popout'],
       title: 'Convenient Effects',
       popOut: true,
       width: 300,
-      height: 900,
+      height: 600,
       resizable: true,
       template:
         'modules/dfreds-convenient-effects/templates/convenient-effects.html',
@@ -17,65 +18,157 @@ export default class ConvenientEffectsApp extends Application {
 
   constructor() {
     super();
+    this._settings = new Settings();
     this._dynamicEffectsAdder = new DynamicEffectsAdder();
   }
 
   getData() {
     const effects = game.dfreds.effects;
+    const expandedFolders = this._settings.expandedFolders;
 
     return {
       folders: [
         {
+          label: 'Favorites',
+          expanded: expandedFolders.includes('Favorites'),
+          effects: this._fetchFavorites(),
+        },
+        {
           label: 'Conditions',
+          expanded: expandedFolders.includes('Conditions'),
           effects: effects.conditions,
         },
         {
           label: 'Spells',
+          expanded: expandedFolders.includes('Spells'),
           effects: effects.spells,
         },
         {
           label: 'Other',
+          expanded: expandedFolders.includes('Other'),
           effects: effects.other,
         },
       ],
     };
   }
 
-  /** @override */
-  activateListeners(html) {
-    const directory = html.find('.directory-list');
-    directory.on('click', '.folder-header', this._toggleFolder.bind(this));
+  _fetchFavorites() {
+    return this._settings.favoriteEffectNames.map((name) => {
+      return game.dfreds.effects.all.find((effect) => effect.name == name);
+    }).sort((a, b) => {
+      let nameA = a.name.toLowerCase();
+      let nameB = b.name.toLowerCase();
 
-    const effectListItem = html.find('.convenient-effect');
-    Array.from(effectListItem).forEach((listItem) => {
-      listItem.addEventListener(
-        'click',
-        this._handleClickingListItem.bind(this)
-      );
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
     });
   }
 
-  _toggleFolder(event) {
+  /** @override */
+  activateListeners(html) {
+    const directory = html.find('.directory-list');
+
+    // Drag events
+    directory.on(
+      'dragstart',
+      '.convenient-effect',
+      this._onEffectDragStart.bind(this)
+    );
+    directory.on(
+      'dragover',
+      '.folder',
+      this._onFolderDragOverOrDragEnter.bind(this)
+    );
+    directory.on(
+      'dragenter',
+      '.folder',
+      this._onFolderDragOverOrDragEnter.bind(this)
+    );
+    directory.on(
+      'dragleave',
+      '.folder',
+      this._onFolderDragLeave.bind(this)
+    );
+    directory.on('drop', '.folder', this._onDropOntoFolder.bind(this));
+
+    // Click handlers
+    directory.on('click', '.folder-header', this._onFolderClick.bind(this));
+    directory.on(
+      'click',
+      '.convenient-effect',
+      this._onEffectClick.bind(this)
+    );
+  }
+
+  _onEffectDragStart(event) {
+    const effectName = event.target.id;
+    event.originalEvent.dataTransfer.setData('text/plain', effectName);
+  }
+
+  _onFolderDragOverOrDragEnter(event) {
+    if (event.currentTarget.dataset.folderLabel != 'Favorites') {
+      return;
+    }
+    event.preventDefault();
+    event.currentTarget.classList.add('droptarget');
+  }
+
+  _onFolderDragLeave(event) {
+    if (event.currentTarget.dataset.folderLabel != 'Favorites') {
+      return;
+    }
+    event.preventDefault();
+    event.currentTarget.classList.remove('droptarget');
+  }
+
+  _onDropOntoFolder(event) {
+    if (event.currentTarget.dataset.folderLabel != 'Favorites') {
+      return;
+    }
+    event.currentTarget.classList.remove('droptarget');
+
+    const effectName = event.originalEvent.dataTransfer.getData('text/plain');
+
+    // Don't add favorites twice
+    if (this._settings.favoriteEffectNames.includes(effectName)) return;
+
+    const draggable = document.getElementById(effectName).cloneNode(true);
+
+    // Add the element to the subdirectory list
+    let folder = $(event.currentTarget);
+    folder.find('.subdirectory').append(draggable);
+
+    this._settings.addFavoriteEffect(effectName);
+
+    // Forces redraw and lose scroll position...
+    // this.render();
+  }
+
+  _onFolderClick(event) {
+    let nameOfFolder = event.currentTarget.parentElement.dataset.folderLabel;
     let folder = $(event.currentTarget.parentElement);
     let collapsed = folder.hasClass('collapsed');
 
     if (collapsed) {
+      this._settings.addExpandedFolder(nameOfFolder);
       folder.removeClass('collapsed');
     } else {
+      this._settings.removeExpandedFolder(nameOfFolder);
       folder.addClass('collapsed');
     }
   }
 
-  async _handleClickingListItem(event) {
+  async _onEffectClick(event) {
     const toggledEffect = game.dfreds.effects.all.find(
       (effect) =>
         effect.name == event.target.innerText ||
         effect.name == event.target.title
     );
-    await this._handleTogglingEffect(toggledEffect);
+    await this._toggleEffect(toggledEffect);
   }
 
-  async _handleTogglingEffect(toggledEffect) {
+  async _toggleEffect(toggledEffect) {
     const controlledTokens = canvas.tokens.controlled;
 
     if (controlledTokens.length === 0) {
--- scripts/settings.js
+++ scripts/settings.js
@@ -3,6 +3,8 @@ export default class Settings {
 
   // Settings keys
   static ALLOW_FOR_PLAYERS = 'allowForPlayers';
+  static FAVORITE_EFFECT_NAMES = 'favoriteEffectNames';
+  static EXPANDED_FOLDERS = 'expandedFolders';
 
   /**
    * Register all the settings for the module
@@ -10,14 +12,37 @@ export default class Settings {
   registerSettings() {
     game.settings.register(Settings.PACKAGE_NAME, Settings.ALLOW_FOR_PLAYERS, {
       name: 'Players Can See',
-      hint:
-        'If enabled, players can see the effects and toggle them on or off.',
+      hint: 'If enabled, players can see the effects and toggle them on or off.',
       scope: 'world',
       config: true,
       default: false,
       type: Boolean,
-      onChange: () => window.location.reload()
+      onChange: () => window.location.reload(),
     });
+
+    game.settings.register(
+      Settings.PACKAGE_NAME,
+      Settings.FAVORITE_EFFECT_NAMES,
+      {
+        name: 'Favorite Effect Names',
+        scope: 'client',
+        config: false,
+        default: '',
+        type: String,
+      }
+    );
+
+    game.settings.register(
+      Settings.PACKAGE_NAME,
+      Settings.EXPANDED_FOLDERS,
+      {
+        name: 'Expanded Folders',
+        scope: 'client',
+        config: false,
+        default: 'Favorites',
+        type: String,
+      }
+    );
   }
 
   /**
@@ -27,5 +52,46 @@ export default class Settings {
    */
   get allowForPlayers() {
     return game.settings.get(Settings.PACKAGE_NAME, Settings.ALLOW_FOR_PLAYERS);
+  }
+
+  /**
+   * Returns the game setting for the favorite effect names
+   *
+   * @returns {Array} the names of all the favorite effects
+   */
+  get favoriteEffectNames() {
+    return game.settings.get(Settings.PACKAGE_NAME, Settings.FAVORITE_EFFECT_NAMES).split(';').filter(name => name.trim());
+  }
+
+  addFavoriteEffect(name) {
+    let favoriteEffectsArray = this.favoriteEffectNames;
+    favoriteEffectsArray.push(name);
+
+    favoriteEffectsArray = [...new Set(favoriteEffectsArray)]; // remove duplicates
+
+    game.settings.set(Settings.PACKAGE_NAME, Settings.FAVORITE_EFFECT_NAMES, favoriteEffectsArray.join(';'));
+  }
+
+  removeFavoriteEffect(name) {
+    let favoriteEffectsArray = this.favoriteEffectNames.filter(favoriteEffect => favoriteEffect !== name);
+    game.settings.set(Settings.PACKAGE_NAME, Settings.FAVORITE_EFFECT_NAMES, favoriteEffectsArray.join(';'));
+  }
+
+  get expandedFolders() {
+    return game.settings.get(Settings.PACKAGE_NAME, Settings.EXPANDED_FOLDERS).split(';').filter(name => name.trim());
+  }
+
+  addExpandedFolder(name) {
+    let expandedFolderArray = this.expandedFolders;
+    expandedFolderArray.push(name);
+
+    expandedFolderArray = [...new Set(expandedFolderArray)]; // remove duplicates
+
+    game.settings.set(Settings.PACKAGE_NAME, Settings.EXPANDED_FOLDERS, expandedFolderArray.join(';'));
+  }
+
+  removeExpandedFolder(name) {
+    let expandedFolderArray = this.expandedFolders.filter(expandedFolder => expandedFolder !== name);
+    game.settings.set(Settings.PACKAGE_NAME, Settings.EXPANDED_FOLDERS, expandedFolderArray.join(';'));
   }
 }
