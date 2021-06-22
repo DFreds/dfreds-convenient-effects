817bbb468e7cb3e590296501f31f61c9ad601cbb
Derek Fredrickson <dfreds08@gmail.com>
2021-06-21 22:36:27 -0400

    Add deprecation warning to toggle effect

--- scripts/effects/toggle-effect.js
+++ scripts/effects/toggle-effect.js
@@ -6,7 +6,9 @@ import EffectHandler from './effect-handler.js';
  * @param {string} name - name of the effect to toggle
  */
 export default async function toggleEffect(name) {
-  // ui.notifications.warn('This is deprecated and will be removed in the future. Re-import the macro to fix this issue.');
+  ui.notifications.warn(
+    'This is deprecated and will be removed in the future. Re-import the convenient effects macro to see how to fix this issue.'
+  );
   const effectHandler = new EffectHandler();
   await effectHandler.toggleEffect(name);
 }
