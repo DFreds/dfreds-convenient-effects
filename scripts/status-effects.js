f61f95a9e519a4ed6756a7b68ebc509967b5a9d3
Derek Fredrickson <dfreds08@gmail.com>
2021-07-15 00:54:37 -0400

    Refactor status effects so it handles onToggleEffect

--- scripts/status-effects.js
+++ scripts/status-effects.js
@@ -1,13 +1,16 @@
 import Settings from './settings.js';
 
 /**
- * Initializes the status effects present on the token HUD
+ * Handles the status effects present on the token HUD
  */
 export default class StatusEffects {
   constructor() {
     this._settings = new Settings();
   }
 
+  /**
+   * Initialize the token status effects based on the user configured settings.
+   */
   initializeStatusEffects() {
     const conditions = game.dfreds.effects.conditions;
     const statusEffectType = this._settings.statusEffectType;
@@ -22,6 +25,28 @@ export default class StatusEffects {
           return condition.convertToActiveEffectData();
         })
       );
+    }
+  }
+
+  /**
+   * This function is called when a token status effect is toggled. If the
+   * status effect is one added by the convenient effect module, it is handled
+   * here. Otherwise, the original wrapper function is used.
+   *
+   * @param {Token5e} token - the token to toggle the effect on
+   * @param {fn} wrapper - the original onToggleEffect function
+   * @param {any[]} args - any arguments provided with the original onToggleEffect function
+   */
+  onToggleEffect({ token, wrapper, args }) {
+    const [event] = args;
+    const statusEffectId = event.currentTarget.dataset.statusId;
+    if (statusEffectId.startsWith('Convenient Effect: ')) {
+      event.preventDefault();
+      event.stopPropagation();
+      const effectName = statusEffectId.replace('Convenient Effect: ', '');
+      game.dfreds.effectHandler.toggleStatusEffect(effectName, token);
+    } else {
+      wrapper(...args);
     }
   }
 }
