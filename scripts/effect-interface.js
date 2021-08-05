f7b5133eba2d75c607e5df93c34fa5627793aa07
Derek Fredrickson <dfreds08@gmail.com>
2021-08-05 18:52:50 -0400

    Add method and class documentation

--- scripts/effect-interface.js
+++ scripts/effect-interface.js
@@ -2,6 +2,9 @@ import ActorUpdater from './effects/actor-updater.js';
 import EffectHandler from './effects/effect-handler.js';
 import FoundryHelpers from './foundry-helpers.js';
 
+/**
+ * Interface for working with effects and executing them as a GM via sockets
+ */
 export default class EffectInterface {
   constructor() {
     this._actorUpdater = new ActorUpdater();
@@ -9,6 +12,9 @@ export default class EffectInterface {
     this._foundryHelpers = new FoundryHelpers();
   }
 
+  /**
+   * Initializes the socket and registers the socket functions
+   */
   initialize() {
     this._socket = socketlib.registerModule('dfreds-convenient-effects');
     this._registerFunctions();
@@ -37,6 +43,13 @@ export default class EffectInterface {
     );
   }
 
+  /**
+   * Toggles the effect on the provided actor UUIDS as the GM via sockets
+   *
+   * @param {string} effectName - the name of the effect to toggle
+   * @param  {...string} uuids - the UUIDs of the actors to toggle the effect on
+   * @returns {Promise} a promise that resolves when the GM socket function completes
+   */
   async toggleEffect(effectName, ...uuids) {
     if (uuids.length == 0) {
       uuids = this._foundryHelpers.getActorUuidsFromCanvas();
@@ -63,6 +76,13 @@ export default class EffectInterface {
     return this._socket.executeAsGM('toggleEffect', effect.name, ...uuids);
   }
 
+  /**
+   * Removes the effect from the provided actor UUID as the GM via sockets
+   *
+   * @param {string} effectName - the name of the effect to remove
+   * @param {string} uuid - the UUID of the actor to remove the effect from
+   * @returns {Promise} a promise that resolves when the GM socket function completes
+   */
   async removeEffect(effectName, uuid) {
     let effect = this._effectHandler.findEffectByName(effectName);
 
@@ -85,6 +105,13 @@ export default class EffectInterface {
     return this._socket.executeAsGM('removeEffect', effect.name, uuid);
   }
 
+  /**
+   * Adds the effect to the provided actor UUID as the GM via sockets
+   *
+   * @param {string} effectName - the name of the effect to add
+   * @param {string} uuid - the UUID of the actor to add the effect to
+   * @returns {Promise} a promise that resolves when the GM socket function completes
+   */
   async addEffect(effectName, uuid, origin) {
     let effect = this._effectHandler.findEffectByName(effectName);
 
@@ -107,10 +134,24 @@ export default class EffectInterface {
     return this._socket.executeAsGM('addEffect', effect.name, uuid, origin);
   }
 
+  /**
+   * Adds data changes to the provided actor UUID as the GM via sockets
+   *
+   * @param {string} effectName - the name of the effect that is adding actor data changes
+   * @param {string} uuid - the UUID of the actor to add the data changes to
+   * @returns {Promise} a promise that resolves when the GM socket function completes
+   */
   addActorDataChanges(effectName, uuid) {
     return this._socket.executeAsGM('addActorDataChanges', effectName, uuid);
   }
 
+  /**
+   * Removes data changes from the provided actor UUID as the GM via sockets
+   *
+   * @param {string} effectName - the name of the effect that is removing actor data changes
+   * @param {string} uuid - the UUID of the actor to remove the data changes from
+   * @returns {Promise} a promise that resolves when the GM socket function completes
+   */
   removeActorDataChanges(effectName, uuid) {
     return this._socket.executeAsGM('removeActorDataChanges', effectName, uuid);
   }
