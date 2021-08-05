30ac7b74e786fe0d812ee4929cdf6a135f77bd6d
Derek Fredrickson <dfreds08@gmail.com>
2021-08-05 13:20:05 -0400

    Update effect handler to run add and remove effect as GM via sockets

--- scripts/effects/effect-handler.js
+++ scripts/effects/effect-handler.js
@@ -1,6 +1,7 @@
 import DynamicEffectsAdder from './dynamic-effects-adder.js';
 import Settings from '../settings.js';
 import log from '../logger.js';
+import socketInstance from '../socket.js';
 
 /**
  * Handles toggling on and off effects on actors
@@ -12,70 +13,22 @@ export default class EffectHandler {
   }
 
   /**
-   * Toggles an effect on or off by name
-   *
-   * @param {string} effectName - name of the effect to toggle
-   * @param {string[]} identifiers - optional ids to apply the effect to. If not provided, it will use the targeted or selected tokens
-   */
-  async toggleEffect(effectName, ...identifiers) {
-    const actorsToEffect = this._determineActorsToEffect(identifiers);
-    if (!actorsToEffect || actorsToEffect.length === 0) return;
-
-    await this._toggleEffect(effectName, actorsToEffect);
-  }
-
-  _determineActorsToEffect(identifiers) {
-    const definedIdentifiers = identifiers.filter((identifier) => identifier);
-    if (definedIdentifiers && definedIdentifiers.length > 0) {
-      return [
-        ...this._findPlaceablesThatMatch(identifiers),
-        ...this._findActorsThatMatch(identifiers),
-      ];
-    }
-
-    if (canvas.tokens.controlled.length == 0 && game.user.targets.size == 0) {
-      ui.notifications.error(
-        'Please select or target a token to apply an effect'
-      );
-      return;
-    }
-
-    if (this._settings.prioritizeTargets && game.user.targets.size !== 0) {
-      return Array.from(game.user.targets).map((token) => token.actor);
-    } else {
-      return canvas.tokens.controlled.map((token) => token.actor);
-    }
-  }
-
-  _findPlaceablesThatMatch(identifiers) {
-    return identifiers
-      .flatMap((identifier) => {
-        return canvas.tokens.placeables.filter((placeable) => {
-          return placeable.id === identifier || placeable.name === identifier;
-        });
-      })
-      .map((token) => token.actor);
-  }
-
-  _findActorsThatMatch(identifiers) {
-    return identifiers.flatMap((identifier) => {
-      return game.actors.filter((actor) => {
-        return actor.uuid === identifier || actor.id === identifier;
-      });
-    });
-  }
-
-  /**
    * Toggles an effect on or off via the token HUD
    *
    * @param {string} effectName - name of the effect to toggle
    * @param {Token5e} token - token to apply the effect to
    */
   async toggleStatusEffect(effectName, token) {
-    await this._toggleEffect(effectName, [token.actor]);
+    await this.toggleEffect(effectName, token.id);
   }
 
-  async _toggleEffect(effectName, actors) {
+  /**
+   * Toggles an effect on or off by name
+   *
+   * @param {string} effectName - name of the effect to toggle
+   * @param {string[]} identifiers - optional identifiers to apply the effect to. If not provided, it will use the targeted or selected tokens
+   */
+  async toggleEffect(effectName, ...identifiers) {
     let effect = this._findEffectByName(effectName);
 
     if (!effect) {
@@ -83,108 +36,44 @@ export default class EffectHandler {
       return;
     }
 
+    if (identifiers.length === 0) {
+      identifiers = this._getTokenIdsFromCanvas();
+    }
+
+    if (identifiers.length == 0) {
+      ui.notifications.error(
+        `Please select or target a token to toggle ${effectName}`
+      );
+      return;
+    }
+
     if (effect.nestedEffects.length > 0) {
       effect = await this._getNestedEffectSelection(effect);
     }
 
-    for (const actor of actors) {
-      if (this.hasEffectApplied(effect.name, actor)) {
-        await this.removeEffect(effect.name, actor);
+    for (const identifier of identifiers) {
+      if (this.hasEffectApplied(effect.name, identifier)) {
+        await this.removeEffect(effect.name, identifier);
       } else {
-        await this.addEffect({ effectName: effect.name, actor });
+        await this.addEffect(effect.name, identifier);
       }
     }
   }
 
-  /**
-   * Checks a provided actor to see if any of its current active effects are a convenient effect
-   *
-   * @param {string} effectName - name of the effect to check
-   * @param {Actor5e} actor - the actor to check
-   * @returns {boolean} true if the effect is applied, false otherwise
-   */
-  hasEffectApplied(effectName, actor) {
-    return actor.data.effects.some(
-      (activeEffect) =>
-        activeEffect?.data?.flags?.isConvenient &&
-        activeEffect?.data?.label == effectName
-    );
-  }
-
-  /**
-   * Removes a convenient effect matching the provided name from an actor if the
-   * effect exists on it
-   *
-   * @param {string} effectName - name of the effect to remove
-   * @param {Actor5e} actor - the actor to remove the effect from
-   */
-  async removeEffect(effectName, actor) {
-    let effect = this._findEffectByName(effectName);
-
-    if (!effect) {
-      ui.notifications.error(`Effect ${effectName} was not found`);
-      return;
-    }
-
-    if (!actor) {
-      ui.notifications.error('Actor must be defined');
-      return;
-    }
-
-    if (effect.nestedEffects.length > 0) {
-      effect = await this._getNestedEffectSelection(effect);
-    }
-
-    const effectToRemove = actor.data.effects.find(
-      (activeEffect) =>
-        activeEffect?.data?.flags?.isConvenient &&
-        activeEffect?.data?.label == effect.name
-    );
-
-    if (effectToRemove) {
-      await actor.deleteEmbeddedDocuments('ActiveEffect', [effectToRemove.id]);
-      log(`Removed effect ${effect.name} from ${actor.name} - ${actor.id}`);
-    }
-  }
-
-  /**
-   * Adds a convenient effect matching the provided name to an actor
-   *
-   * @param {string} effectName - name of the effect to add
-   * @param {Actor5e} actor - the actor to add the effect to
-   * @param {string} origin - the origin to add to the effect
-   */
-  async addEffect({ effectName, actor, origin }) {
-    let effect = this._findEffectByName(effectName);
-
-    if (!effect) {
-      ui.notifications.error(`Effect ${effectName} was not found`);
-      return;
-    }
-
-    if (!actor) {
-      ui.notifications.error('Actor must be defined');
-      return;
-    }
-
-    if (effect.nestedEffects.length > 0) {
-      effect = await this._getNestedEffectSelection(effect);
-    }
-
-    if (effect.isDynamic) {
-      await this._dynamicEffectsAdder.addDynamicEffects(effect, actor);
-    }
-
-    this._handleIntegrations(effect);
-
-    const activeEffectData = effect.convertToActiveEffectData(origin);
-    await actor.createEmbeddedDocuments('ActiveEffect', [activeEffectData]);
-
-    log(`Added effect ${effect.name} to ${actor.name} - ${actor.id}`);
-  }
-
   _findEffectByName(effectName) {
     return game.dfreds.effects.all.find((effect) => effect.name == effectName);
+  }
+
+  _getTokenIdsFromCanvas() {
+    if (canvas.tokens.controlled.length == 0 && game.user.targets.size == 0) {
+      return [];
+    }
+
+    if (this._settings.prioritizeTargets && game.user.targets.size !== 0) {
+      return Array.from(game.user.targets).map((token) => token.id);
+    } else {
+      return canvas.tokens.controlled.map((token) => token.id);
+    }
   }
 
   async _getNestedEffectSelection(effect) {
@@ -208,6 +97,138 @@ export default class EffectHandler {
     return effect.nestedEffects.find(
       (nestedEffect) => nestedEffect.name == choice
     );
+  }
+
+  /**
+   * Checks a provided actor to see if any of its current active effects are a convenient effect
+   *
+   * @param {string} effectName - the name of the effect to check
+   * @param {string} identifier - the identifier to search for. Can be a token
+   * name, token ID, actor ID, or actor UUID
+   * @returns {boolean} true if the effect is applied, false otherwise
+   */
+  hasEffectApplied(effectName, identifier) {
+    let actor = this._findActorFromIdentifier(identifier);
+    return actor?.data?.effects?.some(
+      (activeEffect) =>
+        activeEffect?.data?.flags?.isConvenient &&
+        activeEffect?.data?.label == effectName
+    );
+  }
+
+  _findActorFromIdentifier(identifier) {
+    return (
+      this._findGameActorThatMatches(identifier) ??
+      this._findCanvasActorThatMatches(identifier)
+    );
+  }
+
+  _findGameActorThatMatches(identifier) {
+    return game.actors.find(
+      (actor) => actor.id === identifier || actor.uuid === identifier
+    );
+  }
+
+  _findCanvasActorThatMatches(identifier) {
+    return canvas.tokens.placeables.find(
+      (placeable) =>
+        placeable.id === identifier || placeable.name === identifier
+    )?.actor;
+  }
+
+  /**
+   * Removes a convenient effect matching the provided name from an actor if the
+   * effect exists on it
+   *
+   * @param {string} effectName - the name of the effect to remove
+   * @param {string} identifier - the identifier to search for. Can be a token
+   * name, token ID, actor ID, or actor UUID
+   */
+  async removeEffect(effectName, identifier) {
+    socketInstance.socket.executeAsGM(
+      'removeEffectAsGM',
+      effectName,
+      identifier
+    );
+  }
+
+  async removeEffectAsGM(effectName, identifier) {
+    let effect = this._findEffectByName(effectName);
+
+    if (!effect) {
+      ui.notifications.error(`Effect ${effectName} could not be found`);
+      return;
+    }
+
+    let actor = this._findActorFromIdentifier(identifier);
+
+    if (!actor) {
+      ui.notifications.error(`Actor ${identifier} could not be found`);
+      return;
+    }
+
+    if (effect.nestedEffects.length > 0) {
+      effect = await this._getNestedEffectSelection(effect);
+    }
+
+    const effectToRemove = actor.data.effects.find(
+      (activeEffect) =>
+        activeEffect?.data?.flags?.isConvenient &&
+        activeEffect?.data?.label == effect.name
+    );
+
+    if (effectToRemove) {
+      await actor.deleteEmbeddedDocuments('ActiveEffect', [effectToRemove.id]);
+      log(`Removed effect ${effect.name} from ${actor.name} - ${actor.id}`);
+    }
+  }
+
+  /**
+   * Adds a convenient effect matching the provided name to an actor
+   *
+   * @param {string} effectName - the name of the effect to add
+   * @param {string} identifier - the identifier to search for. Can be a token
+   * name, token ID, actor ID, or actor UUID
+   * @param {string} origin - the origin to add to the effect
+   */
+  async addEffect(effectName, identifier, origin) {
+    socketInstance.socket.executeAsGM(
+      'addEffectAsGM',
+      effectName,
+      identifier,
+      origin
+    );
+  }
+
+  async addEffectAsGM(effectName, identifier, origin) {
+    let effect = this._findEffectByName(effectName);
+
+    if (!effect) {
+      ui.notifications.error(`Effect ${effectName} could not be found`);
+      return;
+    }
+
+    let actor = this._findActorFromIdentifier(identifier);
+
+    if (!actor) {
+      ui.notifications.error(`Actor ${identifier} could not be found`);
+      return;
+    }
+
+    if (effect.nestedEffects.length > 0) {
+      effect = await this._getNestedEffectSelection(effect);
+    }
+
+    if (effect.isDynamic) {
+      await this._dynamicEffectsAdder.addDynamicEffects(effect, actor);
+    }
+
+    this._handleIntegrations(effect);
+
+    const activeEffectData = effect.convertToActiveEffectData(origin);
+    await actor.createEmbeddedDocuments('ActiveEffect', [activeEffectData]);
+
+    log(`Added effect ${effect.name} to ${actor.name} - ${actor.id}`);
   }
 
   _handleIntegrations(effect) {
