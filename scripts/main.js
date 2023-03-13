0239ea42fce4ee4a93d904974ca93d0ff1b17881
Honzo <honzo.nebro@gmail.com>
2023-03-13 23:39:48 +0100

    Added string on Main.js

--- scripts/main.js
+++ scripts/main.js
@@ -139,7 +139,7 @@ Hooks.on('preCreateActiveEffect', (activeEffect, _config, _userId) => {
   const chatHandler = new ChatHandler();
   chatHandler.createChatForEffect({
     effectName: activeEffect?.label,
-    reason: 'Applied to',
+    reason: game.i18n.localize("Main.AppliedTo"),
     actor: activeEffect?.parent,
     isCreateActiveEffect: true,
   });
@@ -186,7 +186,7 @@ Hooks.on('preDeleteActiveEffect', (activeEffect, _config, _userId) => {
   const chatHandler = new ChatHandler();
   chatHandler.createChatForEffect({
     effectName: activeEffect?.label,
-    reason: isExpired ? 'Expired from' : 'Removed from',
+    reason: isExpired ? game.i18n.localize("Main.ExpiredFrom") : game.i18n.localize("Main.RemovedFrom"),
     actor: activeEffect?.parent,
     isCreateActiveEffect: false,
   });
