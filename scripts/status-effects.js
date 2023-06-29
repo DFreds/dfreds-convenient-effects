7fc64b6ee0007dc6dae2fcb3f0d1151285666a0c
Derek Fredrickson <dfreds08@gmail.com>
2023-06-29 15:07:31 -0400

    Remove overwriting of special status effects

--- scripts/status-effects.js
+++ scripts/status-effects.js
@@ -21,25 +21,12 @@ export default class StatusEffects {
     if (modifyStatusEffects === 'replace') {
       CONFIG.Combat.defeatedStatusId = this._effectsHelper.getId('Dead');
       CONFIG.statusEffects = this._fetchStatusEffects();
-
-      this._setSpecialStatusEffects();
     } else if (modifyStatusEffects === 'add') {
       CONFIG.Combat.defeatedStatusId = this._effectsHelper.getId('Dead');
       CONFIG.statusEffects = CONFIG.statusEffects.concat(
         this._fetchStatusEffects()
       );
-
-      this._setSpecialStatusEffects();
     }
-  }
-
-  _setSpecialStatusEffects() {
-    if (!CONFIG.specialStatusEffects) return;
-
-    CONFIG.specialStatusEffects.DEFEATED = this._effectsHelper.getId('Dead');
-    CONFIG.specialStatusEffects.INVISIBLE =
-      this._effectsHelper.getId('Invisible');
-    CONFIG.specialStatusEffects.BLIND = this._effectsHelper.getId('Blinded');
   }
 
   _fetchStatusEffects() {
