a847b753529144c96c789a632618853d7732547d
Themis <idonotwantspams@gmail.com>
2023-08-19 15:05:50 +0100

    controls.js updates
    - Adding some helpers
    - Removing logic for multiple key combos
    - Adjusting title names

--- scripts/controls.js
+++ scripts/controls.js
@@ -22,27 +22,28 @@ export default class Controls {
     if (!tokenButton) return;
 
     tokenButton.tools.push(this._convenientEffectsAppButton);
-    if (this._settings.unifiedAppButton === 'none') tokenButton.tools.push(this._removeEffectsButton);
+    if (!this._unifiedButton) tokenButton.tools.push(this._removeEffectsButton);
   }
 
   get _convenientEffectsAppButton() {
-    const title = this._settings.unifiedAppButton !== 'none' && game.user.role >= this._settings.removeControlsPermission
-      ? `<center><b>DFreds CE Apps</b></center><hl>
+    const title =
+      this._settings.unifiedAppButton !== 'none' &&
+      game.user.role >= this._settings.removeControlsPermission
+        ? `<center><b>Convenient Effects Apps</b></center><hl>
         <p style="text-align:center"><u>Left mouse click</u>: Opens Add CE</p>
-        <hl><u>${this._settings.unifiedAppButton}+Left mouse click</u>: Opens Update CE`
-      : `Add Convenient Effects`;
+        <hl><u>Shift+Left mouse click</u>: Opens Update CE`
+        : `Add Convenient Effects`;
     return {
       name: 'convenient-effects',
       title,
       icon: 'fas fa-hand-sparkles',
       button: true,
-      visible: game.user.role >= this._settings.appControlsPermission,
+      visible: this._userAppControlsPermission,
       onClick: () => {
-        if (this._settings.unifiedAppButton !== 'none' && game.user.role >= this._settings.removeControlsPermission) {
-          if (!event[this._settings.unifiedAppButton]) this._handleConvenientEffectsClick()
-          else this._handleRemoveEffectsClick()  
-        }
-        else this._handleConvenientEffectsClick()  
+        if (this._unifiedButton && this._userRemoveControlsPermission) {
+          if (!event.shiftKey) this._handleConvenientEffectsClick();
+          else this._handleRemoveEffectsClick();
+        } else this._handleConvenientEffectsClick();
       },
     };
   }
@@ -51,19 +52,31 @@ export default class Controls {
     new ConvenientEffectsApp().render(true);
   }
 
+  async _handleRemoveEffectsClick() {
+    const removeEffectsHandler = new RemoveEffectsHandler();
+    return removeEffectsHandler.handle();
+  }
+
   get _removeEffectsButton() {
     return {
-      name: 'remove-or-toggle-effects',
-      title: 'Remove or Toggle Effects',
+      name: 'remove-convenient-effects',
+      title: 'Remove Convenient Effects',
       icon: 'fas fa-trash-alt',
       button: true,
-      visible: game.user.role >= this._settings.removeControlsPermission,
+      visible: this._userRemoveControlsPermission,
       onClick: this._handleRemoveEffectsClick,
     };
   }
 
-  async _handleRemoveEffectsClick() {
-    const removeEffectsHandler = new RemoveEffectsHandler();
-    return removeEffectsHandler.handle();
+  get _unifiedButton() {
+    return this._settings.unifiedAppButton;
+  }
+
+  get _userAppControlsPermission() {
+    return game.user.role >= this._settings.appControlsPermission;
+  }
+
+  get _userRemoveControlsPermission() {
+    return game.user.role >= this._settings.removeControlsPermission;
   }
 }
