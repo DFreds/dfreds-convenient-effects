6dab4b9a4abd44329dfe2d018cf197381b7381dd
Themis <idonotwantspams@gmail.com>
2023-08-21 11:50:05 +0100

    Update effect-helpers.js
    migration helper function

--- scripts/effects/effect-helpers.js
+++ scripts/effects/effect-helpers.js
@@ -140,4 +140,64 @@ export default class EffectHelpers {
 
     return activeEffect.update({ statuses: statusesArray });
   }
+
+  /**
+   * Migrates the description of Custom Convenient Effects to use the ActiveEffect5e#description field instead of the legacy flags.
+   *
+   * @param {string} id - the ID of the custom effects item
+   * @returns {Promise} a promise that resolves when all the Item's ActiveEffects descriptions have migrated
+   */
+  async _migrateCustomEffectsItemEffectDescriptions(id, migration) {
+    const item = this._settings._findCustomEffectsItem(id);
+    const updates = [];
+    const scope = Constants.MODULE_ID;
+    const key_hasmigrated = Constants.FLAGS.HAS_MIGRATED;
+    for (const e of item.effects) {
+      if (e.getFlag(scope, key_hasmigrated) === migration) continue;
+      const effectDescription = e.description;
+      const flagDescription = e.getFlag(
+        Constants.MODULE_ID,
+        Constants.FLAGS.DESCRIPTION
+      );
+      const legacyDescription =
+        e.flags.convenientDescription ?? e.flags.customEffectDescription;
+      const key_description = Constants.FLAGS.DESCRIPTION;
+      const key_isConvenient = Constants.FLAGS.IS_CONVENIENT;
+      const description =
+        !!effectDescription &&
+        !['Applies Custom Effect', 'Applies custom effects', 'test'].some(
+          (d) => d === effectDescription
+        )
+          ? effectDescription
+          : !!flagDescription &&
+            !['Applies Custom Effect', 'Applies custom effects', 'test'].some(
+              (d) => d === flagDescription
+            )
+          ? flagDescription
+          : !!legacyDescription &&
+            !['Applies Custom Effect', 'Applies custom effects', 'test'].some(
+              (d) => d === legacyDescription
+            )
+          ? legacyDescription
+          : '';
+      updates.push({
+        _id: e.id,
+        description,
+        [`flags.${scope}.${key_isConvenient}`]: true,
+        [`flags.${scope}.${key_hasmigrated}`]: migration,
+        ['flags.-=isConvenient']: null, //remove remnants flags
+        ['flags.-=isCustomConvenient']: null,
+        ['flags.-=convenientDescription']: null,
+        ['flags.-=customEffectDescription']: null,
+        [`flags.${scope}.-=${key_description}`]: null, //removes the 'flags.dfreds-convenient-effects.description'
+      });
+    }
+    if (!updates.length) return true;
+    const results = await item.updateEmbeddedDocuments('ActiveEffect', updates);
+    return console.warn(
+      `${Constants.MODULE_NAME} || Migrated Custom Effects: || ${results.map(
+        (r) => r.name
+      )}`
+    );
+  }
 }
