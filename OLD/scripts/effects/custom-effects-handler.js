891657c6216fc09013f08a1477d7d0fb929c5469
Derek Fredrickson <dfreds08@gmail.com>
2024-07-03 13:51:57 -0400

    More old file formatting

--- OLD/scripts/effects/custom-effects-handler.js
+++ OLD/scripts/effects/custom-effects-handler.js
@@ -1,230 +1,237 @@
-import EffectHelpers from './effect-helpers.js';
-import FoundryHelpers from '../util/foundry-helpers.js';
-import Settings from '../settings.js';
-import log from '../logger.js';
+import EffectHelpers from "./effect-helpers.js";
+import FoundryHelpers from "../util/foundry-helpers.js";
+import Settings from "../settings.js";
+import log from "../logger.js";
 
 /**
  * Handles initializing, creating, editing, and deleting custom effects.
  */
 export default class CustomEffectsHandler {
-  constructor() {
-    this._effectHelpers = new EffectHelpers();
-    this._foundryHelpers = new FoundryHelpers();
-    this._settings = new Settings();
-  }
+    constructor() {
+        this._effectHelpers = new EffectHelpers();
+        this._foundryHelpers = new FoundryHelpers();
+        this._settings = new Settings();
+    }
 
-  /**
-   * Checks if a custom effect exists with the provided name
-   *
-   * @param {string} effectName - name of the effect to check if it is custom
-   * @returns {boolean} true if a custom effect exists with the name
-   */
-  isCustomEffect(effectName) {
-    const item = this._findCustomEffectsItem();
-    return item && item.effects.find((effect) => effect.name == effectName);
-  }
+    /**
+     * Checks if a custom effect exists with the provided name
+     *
+     * @param {string} effectName - name of the effect to check if it is custom
+     * @returns {boolean} true if a custom effect exists with the name
+     */
+    isCustomEffect(effectName) {
+        const item = this._findCustomEffectsItem();
+        return item && item.effects.find((effect) => effect.name == effectName);
+    }
 
-  /**
-   * Gets all custom effects
-   *
-   * @returns {ActiveEffect[]} the list of custom effects defined on the custom item
-   */
-  getCustomEffects() {
-    const item = this._findCustomEffectsItem();
-    if (!item) return [];
+    /**
+     * Gets all custom effects
+     *
+     * @returns {ActiveEffect[]} the list of custom effects defined on the custom item
+     */
+    getCustomEffects() {
+        const item = this._findCustomEffectsItem();
+        if (!item) return [];
 
-    let customEffects = Array(...item.effects).map((effect) => {
-      effect.duration.startTime = null;
-      effect.duration.startRound = null;
-      effect.duration.startTurn = null;
-      return effect;
-    });
-    customEffects.sort((a, b) => {
-      let nameA = a.name.toUpperCase(); // ignore upper and lowercase
-      let nameB = b.name.toUpperCase(); // ignore upper and lowercase
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
+        let customEffects = Array(...item.effects).map((effect) => {
+            effect.duration.startTime = null;
+            effect.duration.startRound = null;
+            effect.duration.startTurn = null;
+            return effect;
+        });
+        customEffects.sort((a, b) => {
+            let nameA = a.name.toUpperCase(); // ignore upper and lowercase
+            let nameB = b.name.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
 
-      // names must be equal
-      return 0;
-    });
+            // names must be equal
+            return 0;
+        });
 
-    return customEffects;
-  }
+        return customEffects;
+    }
 
-  /**
-   * Creates a new custom effect on the custom effect item and renders its sheet
-   */
-  async createNewCustomEffect() {
-    const item = await this._findOrCreateCustomEffectsItem();
-    const newEffect = this._effectHelpers.createActiveEffect({
-      name: 'New Effect',
-      origin: item.uuid,
-    });
+    /**
+     * Creates a new custom effect on the custom effect item and renders its sheet
+     */
+    async createNewCustomEffect() {
+        const item = await this._findOrCreateCustomEffectsItem();
+        const newEffect = this._effectHelpers.createActiveEffect({
+            name: "New Effect",
+            origin: item.uuid,
+        });
 
-    const effects = await item.createEmbeddedDocuments('ActiveEffect', [
-      newEffect,
-    ]);
+        const effects = await item.createEmbeddedDocuments("ActiveEffect", [
+            newEffect,
+        ]);
 
-    effects[0].sheet.render(true);
-  }
+        effects[0].sheet.render(true);
+    }
 
-  /**
-   * Creates a new custom effect on the custom effect
-   *
-   * @param {object} params - the params for adding an effect
-   * @param {object[]} params.activeEffects - array of active effects to add
-   * @returns {Promise} a promise that resolves when the active effects have finished being added
-   */
-  async createNewCustomEffectsWith({ activeEffects }) {
-    const item = await this._findOrCreateCustomEffectsItem();
-    const customEffects = activeEffects.map((activeEffect) => {
-      if (!activeEffect.origin) {
-        activeEffect.origin = item.uuid;
-      }
-      return this._effectHelpers.createActiveEffect({ ...activeEffect });
-    });
-    return item.createEmbeddedDocuments('ActiveEffect', customEffects);
-  }
+    /**
+     * Creates a new custom effect on the custom effect
+     *
+     * @param {object} params - the params for adding an effect
+     * @param {object[]} params.activeEffects - array of active effects to add
+     * @returns {Promise} a promise that resolves when the active effects have finished being added
+     */
+    async createNewCustomEffectsWith({ activeEffects }) {
+        const item = await this._findOrCreateCustomEffectsItem();
+        const customEffects = activeEffects.map((activeEffect) => {
+            if (!activeEffect.origin) {
+                activeEffect.origin = item.uuid;
+            }
+            return this._effectHelpers.createActiveEffect({ ...activeEffect });
+        });
+        return item.createEmbeddedDocuments("ActiveEffect", customEffects);
+    }
 
-  /**
-   * Opens the configuration sheet for the custom effect corresponding with the custom ID
-   *
-   * @param {ActiveEffect} effect - the effect to edit
-   */
-  async editCustomEffect(effect) {
-    const item = await this._findCustomEffectsItem();
-    const activeEffect = item.effects.find(
-      (activeEffect) => activeEffect.id === effect.id
-    );
-    activeEffect.sheet.render(true);
-  }
+    /**
+     * Opens the configuration sheet for the custom effect corresponding with the custom ID
+     *
+     * @param {ActiveEffect} effect - the effect to edit
+     */
+    async editCustomEffect(effect) {
+        const item = await this._findCustomEffectsItem();
+        const activeEffect = item.effects.find(
+            (activeEffect) => activeEffect.id === effect.id,
+        );
+        activeEffect.sheet.render(true);
+    }
 
-  /**
-   * Deletes the custom effect corresponding with the custom ID
-   *
-   * @param {ActiveEffect} effect - the effect to delete
-   * @returns {Promise} resolves when the active effect is deleted
-   */
-  async deleteCustomEffect(effect) {
-    const item = await this._findCustomEffectsItem();
-    return item.deleteEmbeddedDocuments('ActiveEffect', [effect.id]);
-  }
+    /**
+     * Deletes the custom effect corresponding with the custom ID
+     *
+     * @param {ActiveEffect} effect - the effect to delete
+     * @returns {Promise} resolves when the active effect is deleted
+     */
+    async deleteCustomEffect(effect) {
+        const item = await this._findCustomEffectsItem();
+        return item.deleteEmbeddedDocuments("ActiveEffect", [effect.id]);
+    }
 
-  /**
-   * Duplicates an exisiting effect
-   *
-   * @param {ActiveEffect} effect - the effect to duplicate
-   */
-  async duplicateExistingEffect(effect) {
-    const item = await this._findOrCreateCustomEffectsItem();
-    const effects = await item.createEmbeddedDocuments('ActiveEffect', [
-      effect,
-    ]);
+    /**
+     * Duplicates an exisiting effect
+     *
+     * @param {ActiveEffect} effect - the effect to duplicate
+     */
+    async duplicateExistingEffect(effect) {
+        const item = await this._findOrCreateCustomEffectsItem();
+        const effects = await item.createEmbeddedDocuments("ActiveEffect", [
+            effect,
+        ]);
 
-    effects[0].sheet.render(true);
-  }
+        effects[0].sheet.render(true);
+    }
 
-  /**
-   * Exports all custom effects on the custom item to JSON
-   */
-  async exportCustomEffectsToJson() {
-    const item = await this._findOrCreateCustomEffectsItem();
-    item.exportToJSON();
-  }
+    /**
+     * Exports all custom effects on the custom item to JSON
+     */
+    async exportCustomEffectsToJson() {
+        const item = await this._findOrCreateCustomEffectsItem();
+        item.exportToJSON();
+    }
 
-  /**
-   * Imports JSON to the custom effects item via a dialog
-   */
-  async importCustomEffectsFromJson() {
-    const item = await this._findOrCreateCustomEffectsItem();
+    /**
+     * Imports JSON to the custom effects item via a dialog
+     */
+    async importCustomEffectsFromJson() {
+        const item = await this._findOrCreateCustomEffectsItem();
 
-    const content = await renderTemplate('templates/apps/import-data.html', {
-      hint1: game.i18n.format('DOCUMENT.ImportDataHint1', {
-        document: item.documentName,
-      }),
-      hint2: game.i18n.format('DOCUMENT.ImportDataHint2', {
-        name: item.name,
-      }),
-    });
-
-    // NOTE: this is taken from foundry, wrapped in a promise by me
-    const importPromise = new Promise((resolve, reject) => {
-      new Dialog(
-        {
-          title: `Import Data: ${item.name}`,
-          content: content,
-          buttons: {
-            import: {
-              icon: '<i class="fas fa-file-import"></i>',
-              label: 'Import',
-              callback: (html) => {
-                const form = html.find('form')[0];
-                if (!form.data.files.length) {
-                  return ui.notifications.error(
-                    'You did not upload a data file!'
-                  );
-                }
-                readTextFromFile(form.data.files[0]).then((json) => {
-                  let jsonData = this._combinePrevAndNewCustomEffects(
-                    item,
-                    json
-                  );
-                  item.importFromJSON(jsonData);
-                  resolve(true);
-                });
-              },
+        const content = await renderTemplate(
+            "templates/apps/import-data.html",
+            {
+                hint1: game.i18n.format("DOCUMENT.ImportDataHint1", {
+                    document: item.documentName,
+                }),
+                hint2: game.i18n.format("DOCUMENT.ImportDataHint2", {
+                    name: item.name,
+                }),
             },
-            no: {
-              icon: '<i class="fas fa-times"></i>',
-              label: 'Cancel',
-            },
-          },
-          default: 'import',
-        },
-        {
-          width: 400,
-        }
-      ).render(true);
-    });
+        );
 
-    await importPromise;
-    this._foundryHelpers.renderConvenientEffectsAppIfOpen();
-  }
+        // NOTE: this is taken from foundry, wrapped in a promise by me
+        const importPromise = new Promise((resolve, reject) => {
+            new Dialog(
+                {
+                    title: `Import Data: ${item.name}`,
+                    content: content,
+                    buttons: {
+                        import: {
+                            icon: '<i class="fas fa-file-import"></i>',
+                            label: "Import",
+                            callback: (html) => {
+                                const form = html.find("form")[0];
+                                if (!form.data.files.length) {
+                                    return ui.notifications.error(
+                                        "You did not upload a data file!",
+                                    );
+                                }
+                                readTextFromFile(form.data.files[0]).then(
+                                    (json) => {
+                                        let jsonData =
+                                            this._combinePrevAndNewCustomEffects(
+                                                item,
+                                                json,
+                                            );
+                                        item.importFromJSON(jsonData);
+                                        resolve(true);
+                                    },
+                                );
+                            },
+                        },
+                        no: {
+                            icon: '<i class="fas fa-times"></i>',
+                            label: "Cancel",
+                        },
+                    },
+                    default: "import",
+                },
+                {
+                    width: 400,
+                },
+            ).render(true);
+        });
 
-  async _findOrCreateCustomEffectsItem() {
-    return (
-      this._findCustomEffectsItem() ?? (await this._createCustomEffectsItem())
-    );
-  }
+        await importPromise;
+        this._foundryHelpers.renderConvenientEffectsAppIfOpen();
+    }
 
-  _findCustomEffectsItem() {
-    return game.items.get(this._settings.customEffectsItemId);
-  }
+    async _findOrCreateCustomEffectsItem() {
+        return (
+            this._findCustomEffectsItem() ??
+            (await this._createCustomEffectsItem())
+        );
+    }
 
-  async _createCustomEffectsItem() {
-    const item = await CONFIG.Item.documentClass.create({
-      name: 'Custom Convenient Effects',
-      img: 'modules/dfreds-convenient-effects/images/magic-palm.svg',
-      type: 'base',
-    });
+    _findCustomEffectsItem() {
+        return game.items.get(this._settings.customEffectsItemId);
+    }
 
-    log(`Creating custom item with ${item.id}`);
-    await this._settings.setCustomEffectsItemId(item.id);
+    async _createCustomEffectsItem() {
+        const item = await CONFIG.Item.documentClass.create({
+            name: "Custom Convenient Effects",
+            img: "modules/dfreds-convenient-effects/images/magic-palm.svg",
+            type: "base",
+        });
 
-    return item;
-  }
+        log(`Creating custom item with ${item.id}`);
+        await this._settings.setCustomEffectsItemId(item.id);
 
-  _combinePrevAndNewCustomEffects(item, json) {
-    let itemDataEffects = Array.from(item.effects);
-    let jsonData = JSON.parse(json);
+        return item;
+    }
 
-    jsonData.effects.push(...itemDataEffects);
+    _combinePrevAndNewCustomEffects(item, json) {
+        let itemDataEffects = Array.from(item.effects);
+        let jsonData = JSON.parse(json);
 
-    return JSON.stringify(jsonData, null, 2);
-  }
+        jsonData.effects.push(...itemDataEffects);
+
+        return JSON.stringify(jsonData, null, 2);
+    }
 }
--- OLD/scripts/effects/effect-helpers.js
+++ OLD/scripts/effects/effect-helpers.js
@@ -1,162 +1,162 @@
-import Constants from '../constants.js';
-import Settings from '../settings.js';
+import Constants from "../constants.js";
+import Settings from "../settings.js";
 
 export default class EffectHelpers {
-  constructor() {
-    this._settings = new Settings();
-  }
-
-  createActiveEffect({
-    name,
-    description = '',
-    icon = 'icons/svg/aura.svg',
-    duration = {},
-    tint = null,
-    seconds = null,
-    rounds = null,
-    turns = null,
-    isDynamic = false,
-    isViewable = true,
-    flags = {},
-    origin = null,
-    statuses = [],
-    changes = [],
-    atlChanges = [],
-    tokenMagicChanges = [],
-    nestedEffects = [],
-    subEffects = [],
-  }) {
-    if (this._settings.integrateWithAte) {
-      changes.push(...atlChanges);
+    constructor() {
+        this._settings = new Settings();
     }
 
-    if (this._settings.integrateWithTokenMagic) {
-      changes.push(...tokenMagicChanges);
-    }
-
-    let ceFlags = {};
-
-    ceFlags[Constants.MODULE_ID] = {};
-    ceFlags[Constants.MODULE_ID][Constants.FLAGS.IS_CONVENIENT] = true;
-    ceFlags[Constants.MODULE_ID][Constants.FLAGS.IS_DYNAMIC] = isDynamic;
-    ceFlags[Constants.MODULE_ID][Constants.FLAGS.IS_VIEWABLE] = isViewable;
-    ceFlags[Constants.MODULE_ID][Constants.FLAGS.NESTED_EFFECTS] =
-      nestedEffects;
-    ceFlags[Constants.MODULE_ID][Constants.FLAGS.SUB_EFFECTS] = subEffects;
-
-    let effectDuration = isEmpty(duration)
-      ? {
-          rounds,
-          seconds,
-          turns,
+    createActiveEffect({
+        name,
+        description = "",
+        icon = "icons/svg/aura.svg",
+        duration = {},
+        tint = null,
+        seconds = null,
+        rounds = null,
+        turns = null,
+        isDynamic = false,
+        isViewable = true,
+        flags = {},
+        origin = null,
+        statuses = [],
+        changes = [],
+        atlChanges = [],
+        tokenMagicChanges = [],
+        nestedEffects = [],
+        subEffects = [],
+    }) {
+        if (this._settings.integrateWithAte) {
+            changes.push(...atlChanges);
         }
-      : duration;
 
-    // Fixes an issue where statuses would be a set instead of an array
-    let statusesArray = Array.from(statuses);
-    statusesArray.unshift(this.getId(name));
+        if (this._settings.integrateWithTokenMagic) {
+            changes.push(...tokenMagicChanges);
+        }
 
-    let effect = new CONFIG.ActiveEffect.documentClass({
-      changes,
-      description,
-      disabled: false,
-      duration: effectDuration,
-      flags: foundry.utils.mergeObject(ceFlags, flags),
-      icon,
-      name,
-      origin,
-      tint,
-      transfer: false,
-      statuses: statusesArray,
-    });
+        let ceFlags = {};
 
-    return effect;
-  }
+        ceFlags[Constants.MODULE_ID] = {};
+        ceFlags[Constants.MODULE_ID][Constants.FLAGS.IS_CONVENIENT] = true;
+        ceFlags[Constants.MODULE_ID][Constants.FLAGS.IS_DYNAMIC] = isDynamic;
+        ceFlags[Constants.MODULE_ID][Constants.FLAGS.IS_VIEWABLE] = isViewable;
+        ceFlags[Constants.MODULE_ID][Constants.FLAGS.NESTED_EFFECTS] =
+            nestedEffects;
+        ceFlags[Constants.MODULE_ID][Constants.FLAGS.SUB_EFFECTS] = subEffects;
 
-  /**
-   * Gets the description attached to the active effect
-   *
-   * @param {ActiveEffect} activeEffect - the active effect
-   * @returns {string} The description for the effect
-   */
-  getDescription(activeEffect) {
-    const effectDescription = activeEffect.description;
-    const flagDescription = activeEffect.getFlag(
-      Constants.MODULE_ID,
-      Constants.FLAGS.DESCRIPTION
-    );
-    const legacyDescription = activeEffect.flags.convenientDescription;
+        let effectDuration = isEmpty(duration)
+            ? {
+                  rounds,
+                  seconds,
+                  turns,
+              }
+            : duration;
 
-    return effectDescription || flagDescription || legacyDescription;
-  }
+        // Fixes an issue where statuses would be a set instead of an array
+        let statusesArray = Array.from(statuses);
+        statusesArray.unshift(this.getId(name));
 
-  /**
-   * Gets the ID for a convenient effect using its name
-   *
-   * @param {string} name - the name of the effect
-   * @returns The ID for the effect
-   */
-  getId(name) {
-    return `Convenient Effect: ${name}`;
-  }
+        let effect = new CONFIG.ActiveEffect.documentClass({
+            changes,
+            description,
+            disabled: false,
+            duration: effectDuration,
+            flags: foundry.utils.mergeObject(ceFlags, flags),
+            icon,
+            name,
+            origin,
+            tint,
+            transfer: false,
+            statuses: statusesArray,
+        });
 
-  /**
-   * Gets the `isConvenient` flag on the active effect if it exists
-   *
-   * @param {ActiveEffect} activeEffect - the active effect
-   * @returns {boolean} true if it is a convenient effect and false otherweise
-   */
-  isConvenient(activeEffect) {
-    const isConvenient =
-      activeEffect.getFlag(
-        Constants.MODULE_ID,
-        Constants.FLAGS.IS_CONVENIENT
-      ) ?? false;
-
-    const isOldConvenient = activeEffect.flags.isConvenient;
-    const isOldCustomConvenient = activeEffect.flags.isCustomConvenient;
-
-    return isConvenient || isOldConvenient || isOldCustomConvenient;
-  }
-
-  /**
-   * Updates the convenient effect status ID with the value of the active
-   * effect name
-   *
-   * @param {ActiveEffect} activeEffect - the active effect
-   */
-  async updateStatusId(activeEffect) {
-    const statusId = this.getId(activeEffect.name);
-    let statusesSet = activeEffect.statuses;
-
-    if (statusesSet.has(statusId)) {
-      return;
+        return effect;
     }
 
-    let statusesArray = Array.from(statusesSet).filter(
-      (status) => !status.startsWith('Convenient Effect: ')
-    );
-
-    statusesArray.unshift(statusId);
-
-    return activeEffect.update({ statuses: statusesArray });
-  }
-
-  get effectsByActorMappings() {
-    return canvas.tokens.controlled
-      .filter((token) => {
-        const effects = token.actor.effects.filter(
-          (activeEffect) => activeEffect.isTemporary
+    /**
+     * Gets the description attached to the active effect
+     *
+     * @param {ActiveEffect} activeEffect - the active effect
+     * @returns {string} The description for the effect
+     */
+    getDescription(activeEffect) {
+        const effectDescription = activeEffect.description;
+        const flagDescription = activeEffect.getFlag(
+            Constants.MODULE_ID,
+            Constants.FLAGS.DESCRIPTION,
         );
-        return effects.length > 0;
-      })
-      .map((token) => {
-        const actor = token.actor;
-        const effects = token.actor.effects.filter(
-          (activeEffect) => activeEffect.isTemporary
+        const legacyDescription = activeEffect.flags.convenientDescription;
+
+        return effectDescription || flagDescription || legacyDescription;
+    }
+
+    /**
+     * Gets the ID for a convenient effect using its name
+     *
+     * @param {string} name - the name of the effect
+     * @returns The ID for the effect
+     */
+    getId(name) {
+        return `Convenient Effect: ${name}`;
+    }
+
+    /**
+     * Gets the `isConvenient` flag on the active effect if it exists
+     *
+     * @param {ActiveEffect} activeEffect - the active effect
+     * @returns {boolean} true if it is a convenient effect and false otherweise
+     */
+    isConvenient(activeEffect) {
+        const isConvenient =
+            activeEffect.getFlag(
+                Constants.MODULE_ID,
+                Constants.FLAGS.IS_CONVENIENT,
+            ) ?? false;
+
+        const isOldConvenient = activeEffect.flags.isConvenient;
+        const isOldCustomConvenient = activeEffect.flags.isCustomConvenient;
+
+        return isConvenient || isOldConvenient || isOldCustomConvenient;
+    }
+
+    /**
+     * Updates the convenient effect status ID with the value of the active
+     * effect name
+     *
+     * @param {ActiveEffect} activeEffect - the active effect
+     */
+    async updateStatusId(activeEffect) {
+        const statusId = this.getId(activeEffect.name);
+        let statusesSet = activeEffect.statuses;
+
+        if (statusesSet.has(statusId)) {
+            return;
+        }
+
+        let statusesArray = Array.from(statusesSet).filter(
+            (status) => !status.startsWith("Convenient Effect: "),
         );
 
-        return { actor, effects };
-      });
-  }
+        statusesArray.unshift(statusId);
+
+        return activeEffect.update({ statuses: statusesArray });
+    }
+
+    get effectsByActorMappings() {
+        return canvas.tokens.controlled
+            .filter((token) => {
+                const effects = token.actor.effects.filter(
+                    (activeEffect) => activeEffect.isTemporary,
+                );
+                return effects.length > 0;
+            })
+            .map((token) => {
+                const actor = token.actor;
+                const effects = token.actor.effects.filter(
+                    (activeEffect) => activeEffect.isTemporary,
+                );
+
+                return { actor, effects };
+            });
+    }
 }
