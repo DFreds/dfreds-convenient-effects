320a36a3913f0f5aafb1cc9acea570ae04e34d99
Derek Fredrickson <dfreds08@gmail.com>
2024-07-16 14:57:10 -0400

    Delete old single item effect creation

--- src/ts/hooks/ready.ts
+++ src/ts/hooks/ready.ts
@@ -1,7 +1,5 @@
 import { id as MODULE_ID } from "@static/module.json";
 import { Listener } from "./index.ts";
-import { Settings } from "../settings.ts";
-import { ItemSource } from "types/foundry/common/documents/item.js";
 
 /**
  * Handle creating the Item that will hold the effects
@@ -9,22 +7,7 @@ import { ItemSource } from "types/foundry/common/documents/item.js";
 const Ready: Listener = {
     listen(): void {
         Hooks.once("ready", async () => {
-            const settings = new Settings();
-            if (settings.effectsItemId) return; // Only do this one time
-            if (!game.user.isGM) return; // Only allow GMs to do this
-
-            // TODO extract?
-            const ceData: Omit<
-                DeepPartial<ItemSource>,
-                "_id" | "name" | "type"
-            > & { _id?: Maybe<string>; name: string; type: string } = {
-                name: "Convenient Effects Holder",
-                img: "modules/dfreds-convenient-effects/images/magic-palm.svg",
-                type: CONFIG.Item.typeLabels[0] ?? "consumable", // TODO when undefined... do what?
-            };
-            const item = await Item.create([ceData]);
-
-            await settings.setEffectsItemId(item[0].id);
+            // TODO anything else here?
 
             Hooks.callAll(`${MODULE_ID}.createEffects`);
         });
