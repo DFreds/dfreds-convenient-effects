0998c93a3dbe88d73d1c38101547dd8de1c7b0a8
Derek Fredrickson <dfreds08@gmail.com>
2024-07-21 23:31:35 -0400

    Initialize the API

--- src/ts/hooks/ready.ts
+++ src/ts/hooks/ready.ts
@@ -1,6 +1,7 @@
 import { id as MODULE_ID } from "@static/module.json";
 import { Listener } from "./index.ts";
 import { activateSocketListener } from "../sockets/socket.ts";
+import { EffectInterface } from "../effect-interface.ts";
 
 /**
  * Handle creating the Item that will hold the effects
@@ -9,6 +10,10 @@ const Ready: Listener = {
     listen(): void {
         Hooks.once("ready", async () => {
             activateSocketListener();
+
+            game.dfreds = {
+                effectInterface: new EffectInterface(),
+            };
 
             Hooks.callAll(`${MODULE_ID}.createEffects`);
         });
