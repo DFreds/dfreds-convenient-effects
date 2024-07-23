cedc816365b499304f8b73f087e2e9b64f14586a
Derek Fredrickson <dfreds08@gmail.com>
2024-07-23 00:33:08 -0400

    Save expanded folders from app

--- src/ts/settings.ts
+++ src/ts/settings.ts
@@ -3,10 +3,12 @@ import { id as MODULE_ID } from "@static/module.json";
 class Settings {
     #USER_ROLES: Record<number, string> = {};
 
-    // Settings keys
+    // Config keys
     #APP_CONTROLS_PERMISSION = "controlsPermission";
     // #PRIORITIZE_TARGETS = "prioritizeTargets"; // TODO make this a checkbox in the app?
 
+    // Non-config keys
+    #EXPANDED_FOLDERS = "expandedFolders";
     #RAN_MIGRATIONS = "ranMigrations";
 
     constructor() {
@@ -37,6 +39,7 @@ class Settings {
     }
 
     #registerNonConfigSettings(): void {
+        this.#registerExpandedFolders();
         this.#registerRanMigrations();
     }
 
@@ -50,6 +53,16 @@ class Settings {
             choices: this.#USER_ROLES,
             type: String,
             requiresReload: true,
+        });
+    }
+
+    #registerExpandedFolders(): void {
+        game.settings.register(MODULE_ID, this.#EXPANDED_FOLDERS, {
+            name: "Expanded Folders",
+            scope: "client",
+            config: false,
+            default: [],
+            type: Array,
         });
     }
 
@@ -73,15 +86,90 @@ class Settings {
         );
     }
 
+    /**
+     * Returns the game setting for the saved expanded folder names
+     *
+     * @returns the IDs of all of the saved expanded folders
+     */
+    get expandedFolders(): string[] {
+        return game.settings.get(MODULE_ID, this.#EXPANDED_FOLDERS) as string[];
+    }
+
+    /**
+     * Adds a given folder ID to the saved expanded folders
+     *
+     * @param id - the ID of the folder to add to the saved expanded folders
+     * @returns a promise that resolves when the settings update is complete
+     */
+    async addExpandedFolder(id: string): Promise<unknown> {
+        let expandedFolderArray = this.expandedFolders;
+        expandedFolderArray.push(id);
+
+        expandedFolderArray = [...new Set(expandedFolderArray)]; // remove duplicates
+
+        return game.settings.set(
+            MODULE_ID,
+            this.#EXPANDED_FOLDERS,
+            expandedFolderArray,
+        );
+    }
+
+    /**
+     * Removes a given folder name from the saved expanded folders
+     *
+     * @param id - the ID of the folder to remove from the saved expanded folders
+     * @returns a promise that resolves when the settings update is complete
+     */
+    async removeExpandedFolder(id: string): Promise<unknown> {
+        const expandedFolderArray = this.expandedFolders.filter(
+            (expandedFolder) => expandedFolder !== id,
+        );
+        return game.settings.set(
+            MODULE_ID,
+            this.#EXPANDED_FOLDERS,
+            expandedFolderArray,
+        );
+    }
+
+    /**
+     * Removes all saved expanded folders
+     *
+     * @returns a promise that resolves when the settings update is complete
+     */
+    async clearExpandedFolders(): Promise<unknown> {
+        return game.settings.set(MODULE_ID, this.#EXPANDED_FOLDERS, []);
+    }
+
+    /**
+     * Checks if the given folder name is expanded
+     *
+     * @param id - the folder ID to search for
+     * @returns true if the folder is in the saved expanded folders, false otherwise
+     */
+    isFolderExpanded(id: string): boolean {
+        return this.expandedFolders.includes(id);
+    }
+
+    /**
+     * Returns the game setting for the ran migration keys
+     *
+     * @returns the migration keys of all ran migrations
+     */
     get ranMigrations(): string[] {
         const effectVersionsRun = game.settings.get(
             MODULE_ID,
             this.#RAN_MIGRATIONS,
-        ) as string[] | undefined;
+        ) as string[];
 
-        return effectVersionsRun ?? [];
+        return effectVersionsRun;
     }
 
+    /**
+     * Adds a given migration key to the saved ran migrations
+     *
+     * @param migrationKey - the key to add to the ran migrations
+     * @returns a promise that resolves when the settings update is complete
+     */
     async addRanMigration(migrationKey: string): Promise<unknown> {
         let ranMigrations = this.ranMigrations;
 
@@ -96,6 +184,11 @@ class Settings {
         );
     }
 
+    /**
+     * Clears all saved migrations
+     *
+     * @returns a promise that resolves when the settings update is complete
+     */
     async clearRanMigrations(): Promise<unknown> {
         return game.settings.set(MODULE_ID, this.#RAN_MIGRATIONS, []);
     }
