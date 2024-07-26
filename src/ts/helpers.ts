359762c942a5f36cab6dc9d8c7bc19afa631a525
Derek Fredrickson <dfreds08@gmail.com>
2024-07-26 18:20:52 -0400

    Add ability to create temporary or passive effects

--- src/ts/helpers.ts
+++ src/ts/helpers.ts
@@ -14,6 +14,7 @@ interface ICreateItemAddOns {
 
 interface ICreateEffectAddOns {
     effect: PreCreate<ActiveEffectSource>;
+    isTemporary?: boolean; // TODO determines if we add our own status
     isDynamic?: boolean; // TODO can we remove this??
     isViewable?: boolean;
     atlChanges?: DeepPartial<EffectChangeData>[];
@@ -47,6 +48,7 @@ function createConvenientItem({
 
 function createConvenientEffect({
     effect,
+    isTemporary = true,
     isDynamic = false,
     isViewable = true,
     atlChanges = [],
@@ -65,6 +67,12 @@ function createConvenientEffect({
     ceFlags[MODULE_ID]![FLAGS.SUB_EFFECTS] = subEffects;
 
     effect.flags = foundry.utils.mergeObject(ceFlags, effectFlags);
+
+    const statusesArray = Array.from(effect.statuses ?? []);
+    if (isTemporary) {
+        statusesArray.unshift(`convenient-effect-${effect.name}`.slugify());
+    }
+    effect.statuses = statusesArray;
 
     // const settings = new Settings();
     // if (settings.integrateWithAte) {
