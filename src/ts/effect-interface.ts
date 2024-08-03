2c2f70ebd736eb5d64db9269fea1b5a3a30ee416
Derek Fredrickson <dfreds08@gmail.com>
2024-08-03 02:08:16 -0400

    Remove unnecessary sort in find effect

--- src/ts/effect-interface.ts
+++ src/ts/effect-interface.ts
@@ -177,20 +177,7 @@ class EffectInterface {
             .filter(
                 (effect) =>
                     effect.id === effectId || effect.name === effectName,
-            )
-            .sort((effectA, effectB) => {
-                const nameA = effectA.name.toUpperCase(); // ignore upper and lowercase
-                const nameB = effectB.name.toUpperCase(); // ignore upper and lowercase
-                if (nameA < nameB) {
-                    return -1;
-                }
-                if (nameA > nameB) {
-                    return 1;
-                }
-
-                // names must be equal
-                return 0;
-            });
+            );
 
         if (matchingEffects.length > 1) {
             log(
