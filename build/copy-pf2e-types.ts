d6be4330f803a525399dfb15ba1678e87070a630
Derek Fredrickson <dfreds08@gmail.com>
2024-08-14 17:10:01 -0400

    Update copy pf2e type script to remove directory first

--- build/copy-pf2e-types.ts
+++ build/copy-pf2e-types.ts
@@ -3,10 +3,8 @@ import fsExtra from "fs-extra";
 import path from "path";
 import { pf2eRepoPath } from "../foundryconfig.json";
 
-const sourceDataPath = path.resolve(pf2eRepoPath, "types");
-const destinationDataPath = path.resolve(process.cwd(), "types");
-
-console.log(`Copying ${sourceDataPath} to ${destinationDataPath}.`);
+const sourceDataPath = path.resolve(pf2eRepoPath, "types", "foundry");
+const destinationDataPath = path.resolve(process.cwd(), "types", "foundry");
 
 const sourceRepoPathStats = fs.lstatSync(sourceDataPath, {
     throwIfNoEntry: false,
@@ -16,8 +14,9 @@ if (!sourceRepoPathStats?.isDirectory()) {
     process.exit(1);
 }
 
-if (!fs.existsSync(destinationDataPath)) {
-    fs.mkdirSync(destinationDataPath, { recursive: true });
-}
+console.log(`Cleaning ${destinationDataPath}`);
+fsExtra.removeSync(destinationDataPath);
 
+console.log(`Copying ${sourceDataPath} to ${destinationDataPath}.`);
+fs.mkdirSync(destinationDataPath, { recursive: true });
 fsExtra.copySync(sourceDataPath, destinationDataPath);
