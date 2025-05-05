bef328be6e6b65673eb95cd4fe072539176e5597
Derek Fredrickson <dfreds08@gmail.com>
2025-05-05 13:33:20 -0400

    Fix warning with multiple sockets

--- src/ts/hooks/socketlib.ready.ts
+++ src/ts/hooks/socketlib.ready.ts
@@ -6,13 +6,15 @@ import { EffectInterface } from "../effect-interface.ts";
 const SocketlibReady: Listener = {
     listen(): void {
         Hooks.once("socketlib.ready", () => {
+            const sockets = new Sockets();
+
             game.dfreds = game.dfreds || {};
             game.dfreds.effectInterface = new EffectInterface({
-                sockets: new Sockets(),
+                sockets,
             });
             (game.modules.get(MODULE_ID) as ConvenientEffectsModule).api =
                 new EffectInterface({
-                    sockets: new Sockets(),
+                    sockets,
                 });
         });
     },
