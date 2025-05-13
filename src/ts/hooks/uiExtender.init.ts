80ded7f986c0643a37a7d78145adc7087c9bf81c
Derek Fredrickson <dfreds08@gmail.com>
2025-05-13 17:34:50 -0400

    Register directory instead of scene control

--- src/ts/hooks/uiExtender.init.ts
+++ src/ts/hooks/uiExtender.init.ts
@@ -1,41 +1,18 @@
 import { MODULE_ID } from "../constants.ts";
 import { Listener } from "./index.ts";
-import { Settings } from "../settings.ts";
-// import { ConvenientEffectsApp } from "../app/convenient-effects-app.ts";
 import { ConvenientEffectsV2 } from "../ui/ce-app/convenient-effects-v2.ts";
 
 const UiExtenderInit: Listener = {
     listen(): void {
         Hooks.once("uiExtender.init", (uiExt: any) => {
             const uiExtender = uiExt as UiExtender;
-            const settings = new Settings();
-
-            uiExtender.registerSceneControl({
+            uiExtender.registerDirectory({
                 moduleId: MODULE_ID,
-                name: "tokens",
-                predicate: () => {
-                    return game.user.role >= settings.appControlsPermission;
-                },
-                tool: {
-                    name: "convenientEffects",
-                    title: "ConvenientEffects.AppName",
-                    icon: "fas fa-hand-sparkles",
-                    toolclip: {
-                        src: "modules/dfreds-convenient-effects/images/toolclip-ce.webm",
-                        heading: "ConvenientEffects.AppName",
-                        items: [
-                            {
-                                heading: "ConvenientEffects.AppName",
-                                reference: "CONTROLS.Click",
-                            },
-                        ],
-                    },
-                    button: true,
-                    visible: true,
-                    onChange: (_event?: Event, _active?: boolean) => {
-                        new ConvenientEffectsV2().render({ force: true });
-                    },
-                },
+                id: "convenientEffects",
+                tooltip: "ConvenientEffects.AppName",
+                icon: "fas fa-hand-sparkles",
+                order: 5,
+                applicationClass: ConvenientEffectsV2,
             });
         });
     },
