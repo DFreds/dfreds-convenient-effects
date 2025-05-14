3a9b1d4114d28bab049242bfbb1bcc696e349925
Derek Fredrickson <dfreds08@gmail.com>
2025-05-14 15:48:54 -0400

    Fix header and rendering of sidebar app

--- src/ts/hooks/ready.ts
+++ src/ts/hooks/ready.ts
@@ -2,6 +2,7 @@ import { MODULE_ID } from "../constants.ts";
 import { Mapping } from "../effects/mapping.ts";
 import { error } from "../logger.ts";
 import { Listener } from "./index.ts";
+import { renderApp } from "../ui/render-app-if-open.ts";
 
 /**
  * Handle creating the Item that will hold the effects
@@ -17,6 +18,7 @@ const Ready: Listener = {
             try {
                 await systemDefinition?.effectDefinition?.initialize();
                 Hooks.callAll(`${MODULE_ID}.ready`);
+                renderApp();
             } catch (e: any) {
                 ui.notifications.error(
                     `Something went wrong while initializing convenient effects`,
--- src/ts/ui/render-app-if-open.ts
+++ src/ts/ui/render-app-if-open.ts
@@ -6,12 +6,27 @@ import { ConvenientEffectsV2 } from "./ce-app/convenient-effects-v2.ts";
 function renderAppIfOpen(): void {
     const applications = foundry.applications.instances;
 
-    // @ts-expect-error The types provided by pf2e think this is a number
-    const convenientEffectsV2 = applications.get("convenient-effects-v2");
+    const convenientEffectsV2 = applications.get(
+        // @ts-expect-error The types provided by pf2e think this is a number
+        ConvenientEffectsV2.tabName,
+    ) as ConvenientEffectsV2;
 
-    if (convenientEffectsV2) {
-        (convenientEffectsV2 as ConvenientEffectsV2).render();
+    if (convenientEffectsV2 && convenientEffectsV2.active) {
+        convenientEffectsV2.render();
     }
 }
 
-export { renderAppIfOpen };
+function renderApp(): void {
+    const applications = foundry.applications.instances;
+
+    const convenientEffectsV2 = applications.get(
+        // @ts-expect-error The types provided by pf2e think this is a number
+        ConvenientEffectsV2.tabName,
+    ) as ConvenientEffectsV2;
+
+    if (convenientEffectsV2) {
+        convenientEffectsV2.render();
+    }
+}
+
+export { renderAppIfOpen, renderApp };
