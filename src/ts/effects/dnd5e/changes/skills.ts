52177f0bd3406ada83b3fb8a9bc873a42a044801
Derek Fredrickson <dfreds08@gmail.com>
2026-07-24 21:14:49 -0400

    Add missing skill changes

--- src/ts/effects/dnd5e/changes/skills.ts
+++ src/ts/effects/dnd5e/changes/skills.ts
@@ -54,4 +54,99 @@ function skillCheckMode({
     };
 }
 
-export { skillCheckBonus, skillCheckMode };
+function skillPassiveBonus({
+    skillType,
+    value,
+    priority,
+}: {
+    skillType: SkillType;
+    value: string;
+    priority?: number;
+}): Partial<EffectChangeData> {
+    return {
+        key: `system.skills.${skillType}.bonuses.passive`,
+        type: "add",
+        value,
+        priority,
+    };
+}
+
+// NOTE: Proficiency level: 0 none, 0.5 half, 1 proficient, 2 expertise.
+function skillProficiency({
+    skillType,
+    value,
+    priority,
+}: {
+    skillType: SkillType;
+    value: "0" | "0.5" | "1" | "2";
+    priority?: number;
+}): Partial<EffectChangeData> {
+    return {
+        key: `system.skills.${skillType}.value`,
+        type: "upgrade",
+        value,
+        priority,
+    };
+}
+
+function toolCheckBonus({
+    toolType,
+    value,
+    priority,
+}: {
+    toolType: string;
+    value: string;
+    priority?: number;
+}): Partial<EffectChangeData> {
+    return {
+        key: `system.tools.${toolType}.bonuses.check`,
+        type: "add",
+        value,
+        priority,
+    };
+}
+
+function toolCheckMode({
+    toolType,
+    value,
+    priority,
+}: {
+    toolType: string;
+    value: "-1" | "0" | "1";
+    priority?: number;
+}): Partial<EffectChangeData> {
+    return {
+        key: `system.tools.${toolType}.roll.mode`,
+        type: "add",
+        value,
+        priority,
+    };
+}
+
+function toolProficiency({
+    toolType,
+    value,
+    priority,
+}: {
+    toolType: string;
+    value: "0" | "0.5" | "1" | "2";
+    priority?: number;
+}): Partial<EffectChangeData> {
+    return {
+        key: `system.tools.${toolType}.value`,
+        type: "upgrade",
+        value,
+        priority,
+    };
+}
+
+export {
+    skillCheckBonus,
+    skillCheckMode,
+    skillPassiveBonus,
+    skillProficiency,
+    toolCheckBonus,
+    toolCheckMode,
+    toolProficiency,
+};
+export type { SkillType };
